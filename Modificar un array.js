3d15c790f6516fa178f0afbb8d99c1112bb8deed
Susana Rivera <55016485+susanarivs@users.noreply.github.com>
2021-11-30 13:39:46 -0600

    Completo
    Un mini curso de Java Script

---
+++ Modificar un array.js
@@ -0,0 +1,5 @@
+///Un metodo de ejemplo
+var articulos = ['carro', 'celular', 'moto', 'tv', 'libro']; //modificar moto
+
+articulos.splice(2, 1, 'laptop'); /*se aplican los mismo parámetros, pero esta vez se le agrega el valor al que queremos modificar*/
+//['carro', 'celular', 'laptop', 'tv', 'libro']
---
+++ Variables.js
@@ -0,0 +1,24 @@
+///Que es una variable ?
+/// Es la representacion de algun lugar en memoria que reservamos para poder guardar un valor el cual puede tener diferentes tipos
+var ///representacion donde vamos a guardar valores
+var nombre="Susana"; ///sintaxis para hacer una variable y para poder acceder al valor de la variable es trayendo el nombre de la variable
+
+///declarar generamos la plabra reservada var y le vamos a dar nombre a ese espcio en la memoria
+var edad; // Despues le damos su valor
+//Inicializar el valor 
+edad=30; //traemos el nombre el espaci y le damos un valor
+// si no la inicializo posiblemente sea undefine porque aparta el espacio pero no le da valor
+ //Variables complejas
+ var elementos=["computadora","celular"] //Declaramos e inicializamos una variable
+ // El signo = es un operador que usamos para asignar valor 
+ /// Variable tipo objeto
+
+ var persona={ ///Tenemos una variable que es el espacio donde esta guardado, pero hay mas cosas adentro de diferentes valoresy por lo tanto es una variable compleja 
+     nombre:"Diego", ///La coma se usa para sintaxis de objeto
+     edad:30 ///Ya no uso coma porque finalizo mi objeto
+
+ }
+
+ persona; //Llamar la variable
+///Para entrar a la variable 
+elementos[1]
